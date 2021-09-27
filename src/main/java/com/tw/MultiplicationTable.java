4988ac8ce72f21eba70e329bd5b81da7f1f188a1
jeany <xujin0515@gmail.com>
2021-09-27 14:16:34 +0800

    add test for functions

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,7 +1,31 @@
 package com.tw;
 
 public class MultiplicationTable {
-    public String create(int startNumber, int endNumber) {
+    public String create(int start, int end) {
+        return null;
+    }
+
+    public Boolean isValid(int start, int end) {
+        return null;
+    }
+
+    public Boolean isInRange(int number) {
+        return null;
+    }
+
+    public Boolean isStartNotBiggerThanEnd(int start, int end) {
+        return null;
+    }
+
+    public String generateTable(int start, int end) {
+        return null;
+    }
+
+    public String generateLine(int start, int row) {
+        return null;
+    }
+
+    public String generateSingleExpression(int multiplicand, int multiplier) {
         return null;
     }
 }
--- src/test/java/com/tw/MultiplicationTableTest.java
+++ src/test/java/com/tw/MultiplicationTableTest.java
@@ -2,10 +2,122 @@ package com.tw;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MultiplicationTableTest {
+    @Test
+    void should_return_true_when_validate_given_start_smaller_than_end() {
+        //given
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        int start = 2;
+        int end = 3;
+
+        //when
+        Boolean actual = multiplicationTable.isStartNotBiggerThanEnd(start, end);
+
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    void should_return_false_when_validate_given_start_bigger_than_end() {
+        //given
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        int start = 4;
+        int end = 2;
+
+        //when
+        Boolean actual = multiplicationTable.isStartNotBiggerThanEnd(start, end);
+
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    void should_return_false_when_validate_given_start_0() {
+        //given
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        int start = 0;
+
+        //when
+        Boolean actual = multiplicationTable.isInRange(start);
+
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    void should_return_false_when_validate_given_end_1001() {
+        //given
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        int end = 1001;
+
+        //when
+        Boolean actual = multiplicationTable.isInRange(end);
+
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    void should_return_true_when_validate_given_start_4_and_end_100() {
+        //given
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        int start = 4;
+        int end = 100;
+
+        //when
+        Boolean actual = multiplicationTable.isValid(start, end);
+
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    void should_return_multiplication_expression_when_multiply_given_multiplicand_2_and_multiplier_3() {
+        //given
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        int multiplicand = 2;
+        int multiplier = 3;
+
+        //when
+        String actual = multiplicationTable.generateSingleExpression(multiplicand, multiplier);
+        String expected = "2*3=6";
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_return_multiplication_line_when_multiply_given_start_2_and_row_5() {
+        //given
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        int start = 2;
+        int row = 5;
+
+        //when
+        String actual = multiplicationTable.generateLine(start, row);
+        String expected = "2*5=10  3*5=15  4*5=20  5*5=25";
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_return_multiply_table_when_multiply_given_start_number_2_end_number_5() {
+        //given
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        int start = 2;
+        int end = 5;
+
+        //when
+        String actual = multiplicationTable.generateTable(start, end);
+        String expected = String.format("2*2=4%n2*3=6  3*3=9%n2*4=8  3*4=12  4*4=16%n2*5=10  3*5=15  4*5=20  5*5=25");
+
+        //then
+        assertEquals(expected, actual);
+    }
+
     @Test
     void should_return_null_when_multiply_given_start_number_2_end_number_1() {
         //given
@@ -46,20 +158,5 @@ public class MultiplicationTableTest {
 
         //then
         assertEquals("2*2=4", actual);
-    }
-
-    @Test
-    void should_return_multiply_table_when_multiply_given_start_number_2_end_number_5() {
-        //given
-        MultiplicationTable multiplicationTable = new MultiplicationTable();
-        int start = 2;
-        int end = 5;
-
-        //when
-        String actual = multiplicationTable.create(start, end);
-        String expected = String.format("2*2=4%n2*3=6  3*3=9%n2*4=8  3*4=12  4*4=16%n2*5=10  3*5=15  4*5=20  5*5=25");
-
-        //then
-        assertEquals(expected, actual);
     }
 }
